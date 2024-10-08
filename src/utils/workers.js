317bfb35bec442607da14a40e74712bbc0fffd49
viz-nu <vishnu.teja101.vt@gmail.com>
2024-10-08 11:48:13 +0530

    test: env

--- src/utils/workers.js
+++ src/utils/workers.js
@@ -32,9 +32,9 @@ export const generateCloudinaryQRCode = async (text) => {
         // Generate QR code as a data URL
         const qrCodeData = await QRCode.toDataURL(text);
         cloudinary.config({
-            cloud_name: Process.env.CLOUDINARY_cloud_name,
-            api_key: Process.env.CLOUDINARY_api_key,
-            api_secret: Process.env.CLOUDINARY_api_secret
+            cloud_name: process.env.CLOUDINARY_cloud_name,
+            api_key: process.env.CLOUDINARY_api_key,
+            api_secret: process.env.CLOUDINARY_api_secret
         })
         // Upload the QR code to Cloudinary
         const result = await cloudinary.uploader.upload(qrCodeData, { folder: 'qr_codes', });

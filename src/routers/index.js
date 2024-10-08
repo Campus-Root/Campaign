80f4d4ec8856d79e0e15495e606835d567ba6d3c
viz-nu <vishnu.teja101.vt@gmail.com>
2024-10-08 20:22:04 +0530

    fix: bug

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 const router = Router();
-import { AttendeeRegister, hostRegister, login } from "../controllers/authController.js";
-import { attendeeRegisterSchema } from "../lib/joiSchemas/account.schema.js";
-import { JoiValidator } from "../lib/joiSchemas/index.js";
+import {  hostRegister, login } from "../controllers/authController.js";
+// import { attendeeRegisterSchema } from "../lib/joiSchemas/account.schema.js";
+// import { JoiValidator } from "../lib/joiSchemas/index.js";
 import { auth, isAdmin } from "../middleware/auth.js";
 import { participants, visit } from '../controllers/operations.js';
 
-router.post("/auth/register", JoiValidator(attendeeRegisterSchema), AttendeeRegister);
+// router.post("/auth/register", JoiValidator(attendeeRegisterSchema), AttendeeRegister);
 router.post("/auth/host-register", auth, isAdmin, hostRegister)
 router.post("/auth/host-login", login)
 router.get("/participants", auth, participants)
